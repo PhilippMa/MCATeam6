527f2ff838f895bc055d0c7040ba37f3ae40f975
alex-ander <alex98.kranzer@gmail.com>
2019-09-27 19:26:17 +0900

    init: create project in Android Studio

---
+++ app/src/main/java/com/example/mcateam6/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mcateam6.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mcateam6.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
