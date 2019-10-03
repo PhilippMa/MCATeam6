3849c6ff87b6afdfcdcc66447b4fa7ebe323166b
Yuna Jeong <yoona3316@naver.com>
2019-10-03 21:02:46 +0900

    feat: add nav bar with additional fragments

--- app/src/main/java/com/example/mcateam6/activities/MainActivity.kt
+++ app/src/main/java/com/example/mcateam6/activities/MainActivity.kt
@@ -2,12 +2,52 @@ package com.example.mcateam6.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.get
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.mcateam6.R
+import com.example.mcateam6.fragments.BarcodeFragment
+import com.example.mcateam6.fragments.ProductInfoFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_nav.menu?.get(1).setChecked(true)
+        val fm = supportFragmentManager
+
+        var fragmentTransaction = fm.beginTransaction()
+        fragmentTransaction.add(R.id.layout_root, BarcodeFragment())
+        fragmentTransaction.commit()
+
+        bottom_nav.setOnNavigationItemSelectedListener(object: BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                fragmentTransaction = fm.beginTransaction()
+                when (item.itemId) {
+                    R.id.menu_1 -> {
+                        fragmentTransaction.replace(R.id.layout_root, ProductInfoFragment())
+                    }
+                    R.id.menu_2 -> {
+                        fragmentTransaction.replace(R.id.layout_root, BarcodeFragment())
+                    }
+                    R.id.menu_3 -> {
+
+                    }
+                    R.id.menu_4 -> {
+
+                    }
+                }
+                fragmentTransaction.commit()
+                return true
+            }
+
+        })
     }
 }
