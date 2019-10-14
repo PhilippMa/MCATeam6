3f4342f4db954af15887099e1d76376d05293219
Yuna Jeong <yoona3316@naver.com>
2019-10-14 16:49:01 +0900

    fix: fix bindToLifeCycle in Barcode Fragment
    bug fix for issue #1

--- app/src/main/java/com/example/mcateam6/activities/MainActivity.kt
+++ app/src/main/java/com/example/mcateam6/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.view.get
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -17,7 +18,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var fragmentList = mutableListOf<Fragment>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,8 +26,18 @@ class MainActivity : AppCompatActivity() {
         bottom_nav.menu?.get(1).setChecked(true)
         val fm = supportFragmentManager
 
+        val productInfoFragment = ProductInfoFragment()
+        val barcodeFragment = BarcodeFragment()
+        val searchFragment = SearchFragment()
+        val settingFragment = SettingFragment()
+
+        fragmentList.add(productInfoFragment)
+        fragmentList.add(barcodeFragment)
+        fragmentList.add(searchFragment)
+        fragmentList.add(settingFragment)
+
         var fragmentTransaction = fm.beginTransaction()
-        fragmentTransaction.add(R.id.layout_root, BarcodeFragment())
+        fragmentTransaction.add(R.id.layout_root, barcodeFragment)
         fragmentTransaction.commit()
 
         bottom_nav.setOnNavigationItemSelectedListener(object: BottomNavigationView.OnNavigationItemSelectedListener {
@@ -34,16 +45,16 @@ class MainActivity : AppCompatActivity() {
                 fragmentTransaction = fm.beginTransaction()
                 when (item.itemId) {
                     R.id.menu_1 -> {
-                        fragmentTransaction.replace(R.id.layout_root, ProductInfoFragment())
+                        fragmentTransaction.replace(R.id.layout_root, fragmentList[0])
                     }
                     R.id.menu_2 -> {
-                        fragmentTransaction.replace(R.id.layout_root, BarcodeFragment())
+                        fragmentTransaction.replace(R.id.layout_root, fragmentList[1])
                     }
                     R.id.menu_3 -> {
-                        fragmentTransaction.replace(R.id.layout_root, SearchFragment())
+                        fragmentTransaction.replace(R.id.layout_root, fragmentList[2])
                     }
                     R.id.menu_4 -> {
-                        fragmentTransaction.replace(R.id.layout_root, SettingFragment())
+                        fragmentTransaction.replace(R.id.layout_root, fragmentList[3])
                     }
                 }
                 fragmentTransaction.commit()
