f68ccd28f2331e95ad51add40761c0b0c937153d
alex-ander <alex98.kranzer@gmail.com>
2019-10-15 17:02:06 +0900

    test: more test cases

--- app/src/androidTest/java/com/example/mcateam6/DatabaseInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/mcateam6/DatabaseInstrumentedTest.kt
@@ -4,6 +4,7 @@ package com.example.mcateam6
 import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.mcateam6.database.RemoteDatabase
+import com.example.mcateam6.datatypes.Attribute
 import com.example.mcateam6.datatypes.Product
 import com.google.android.gms.tasks.Tasks
 import org.junit.Assert.*
@@ -41,10 +42,22 @@ class DatabaseInstrumentedTest {
      */
     @Test
     fun test_basicUpload() {
-        val task = db.upload("TestProduct", "표본", "0123456789", "This is a description", emptyList(), emptyMap())
+        basicUpload()
+    }
+
+    private fun basicUpload() : String {
+        val task = db.upload(
+            "TestProduct",
+            "표본",
+            "0123456789",
+            "This is a description",
+            emptyList(),
+            emptyMap()
+        )
         val res = Tasks.await(task, time, timeUnit)
         Log.i("FirebaseDatabase", "ID of document is: " + res.id)
         assertNotNull("Upload failed", res)
+        return res.id
     }
 
     /**
@@ -61,5 +74,36 @@ class DatabaseInstrumentedTest {
         val res = Tasks.await(task, time, timeUnit)
         Log.i("FirebaseDatabase", "ID of document is: " + res.id)
         assertNotNull("Upload failed", res)
+    }
+
+    @Test
+    fun test_uploadDownload_basic() {
+        val id = basicUpload()
+        val res = Tasks.await(db.getByID_Raw(id))
+        assertEquals("id of the downloaded document is different from the uploaded documents", id, res.id)
+    }
+
+    @Test
+    fun test_uploadProductDetailed() {
+        val ingre1 = Product("Ingredient1", "name1", "123123123", "This is a ingredient", emptyList(), mapOf(Pair(Attribute.VEGAN, false), Pair(Attribute.VEGETARIAN, true)))
+        val ingre2 = Product("Ingredient2", "name2", "321321321", "This is a ingredient", emptyList(), mapOf(Pair(Attribute.VEGAN, false), Pair(Attribute.VEGETARIAN, false)))
+        val prod = Product("Product", "name3", "456654456", "This is a product", listOf(ingre1, ingre2), mapOf(Pair(Attribute.VEGAN, false), Pair(Attribute.VEGETARIAN, false)))
+        val task = db.upload(ingre1, ingre2, prod)
+        val res = Tasks.await(task, time, timeUnit)
+        assertNotNull("Upload failed", res)
+    }
+
+    @Test
+    fun test_downloadBasic() {
+        val id = "ocIj7Vfsvd3TeceqZjLR"
+        val res = Tasks.await(db.getByID_Raw(id))
+        assertEquals("id of the downloaded document is different from the uploaded documents", id, res.id)
+    }
+
+    @Test
+    fun test_downloadProduct() {
+        val id = "ocIj7Vfsvd3TeceqZjLR"
+        val res = Tasks.await(db.getProductById(id))
+        assertNotNull("Downloaded product is null", res)
     }
 }
