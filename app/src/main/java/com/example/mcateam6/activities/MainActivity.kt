f7b42513bf03331c1f0d9ee9819b9b9e1317c6ce
Yuna Jeong <yoona3316@naver.com>
2019-10-22 03:32:17 +0900

    fix: change bottom nav

--- app/src/main/java/com/example/mcateam6/activities/MainActivity.kt
+++ app/src/main/java/com/example/mcateam6/activities/MainActivity.kt
@@ -1,66 +1,30 @@
 package com.example.mcateam6.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.core.view.get
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.navigation.Navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mcateam6.R
-import com.example.mcateam6.fragments.BarcodeFragment
-import com.example.mcateam6.fragments.ProductInfoFragment
-import com.example.mcateam6.fragments.SearchFragment
-import com.example.mcateam6.fragments.SettingFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var fragmentList = mutableListOf<Fragment>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottom_nav.menu?.get(1).setChecked(true)
-        val fm = supportFragmentManager
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val productInfoFragment = ProductInfoFragment()
-        val barcodeFragment = BarcodeFragment()
-        val searchFragment = SearchFragment()
-        val settingFragment = SettingFragment()
-
-        fragmentList.add(productInfoFragment)
-        fragmentList.add(barcodeFragment)
-        fragmentList.add(searchFragment)
-        fragmentList.add(settingFragment)
-
-        var fragmentTransaction = fm.beginTransaction()
-        fragmentTransaction.add(R.id.layout_root, barcodeFragment)
-        fragmentTransaction.commit()
-
-        bottom_nav.setOnNavigationItemSelectedListener(object: BottomNavigationView.OnNavigationItemSelectedListener {
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                fragmentTransaction = fm.beginTransaction()
-                when (item.itemId) {
-                    R.id.menu_1 -> {
-                        fragmentTransaction.replace(R.id.layout_root, fragmentList[0])
-                    }
-                    R.id.menu_2 -> {
-                        fragmentTransaction.replace(R.id.layout_root, fragmentList[1])
-                    }
-                    R.id.menu_3 -> {
-                        fragmentTransaction.replace(R.id.layout_root, fragmentList[2])
-                    }
-                    R.id.menu_4 -> {
-                        fragmentTransaction.replace(R.id.layout_root, fragmentList[3])
-                    }
-                }
-                fragmentTransaction.commit()
-                return true
-            }
-
-        })
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
