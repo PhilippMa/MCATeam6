ac25d820bb038d548ae45a80513f2b7e184eb428
Yuna Jeong <yoona3316@naver.com>
2019-10-31 21:12:56 +0900

    feat: add search bar to search fragment #2

--- app/src/main/java/com/example/mcateam6/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/mcateam6/fragments/SearchFragment.kt
@@ -2,17 +2,37 @@ package com.example.mcateam6.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import com.example.mcateam6.R
+import com.mancj.materialsearchbar.MaterialSearchBar
+import kotlinx.android.synthetic.main.fragment_search.*
 
-class SearchFragment : Fragment() {
+
+class SearchFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val v = inflater.inflate(R.layout.fragment_search, container, false)
+        val searchBar = v.findViewById<MaterialSearchBar>(R.id.searchBar)
+        searchBar.inflateMenu(R.menu.search_item_menu)
+        searchBar.menu.setOnMenuItemClickListener(this)
+        return v
+    }
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.item1 -> {
+                searchBar.setHint("Korean")
+            }
+            R.id.item2 -> {
+                searchBar.setHint("English")
+            }
+        }
+        return false
     }
 }
