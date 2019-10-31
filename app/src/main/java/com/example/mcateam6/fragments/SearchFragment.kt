b6019ebfeb363768b8a10444995339194836b71c
Yuna Jeong <yoona3316@naver.com>
2019-10-31 22:12:06 +0900

    feat: add search functionality to search bar

--- app/src/main/java/com/example/mcateam6/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/mcateam6/fragments/SearchFragment.kt
@@ -5,14 +5,25 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import com.example.mcateam6.R
+import com.example.mcateam6.database.RemoteDatabase
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.mancj.materialsearchbar.MaterialSearchBar
 import kotlinx.android.synthetic.main.fragment_search.*
 
 
-class SearchFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
+class SearchFragment : Fragment(), PopupMenu.OnMenuItemClickListener, MaterialSearchBar.OnSearchActionListener {
+    val db = RemoteDatabase()
+    private val ITEM_ALL = 0
+    private val ITEM_KOREAN = 1
+    private val ITEM_ENGLISH = 2
+
+    var itemIndex = 0
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,19 +31,64 @@ class SearchFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
     ): View? {
         val v = inflater.inflate(R.layout.fragment_search, container, false)
         val searchBar = v.findViewById<MaterialSearchBar>(R.id.searchBar)
+        searchBar.setOnSearchActionListener(this)
         searchBar.inflateMenu(R.menu.search_item_menu)
         searchBar.menu.setOnMenuItemClickListener(this)
+
         return v
     }
     override fun onMenuItemClick(item: MenuItem?): Boolean {
         when(item?.itemId) {
-            R.id.item1 -> {
-                searchBar.setHint("Korean")
+            R.id.item_all -> {
+                searchBar.setHint("All")
+                itemIndex = ITEM_ALL
             }
-            R.id.item2 -> {
+            R.id.item_korean -> {
+                searchBar.setHint("Korean")
+                itemIndex = ITEM_KOREAN
+            }
+            R.id.item_english -> {
                 searchBar.setHint("English")
+                itemIndex = ITEM_ENGLISH
             }
         }
         return false
+    }
+    override fun onButtonClicked(buttonCode: Int) {
+        when (buttonCode) {
+            MaterialSearchBar.BUTTON_BACK -> {
+                searchBar.disableSearch()
+            }
+        }
+    }
+
+    override fun onSearchStateChanged(enabled: Boolean) {
+        return
+    }
+
+    override fun onSearchConfirmed(text: CharSequence?) {
+        var task: Task<List<RemoteDatabase.FirebaseProduct>>? = null
+        when (itemIndex) {
+            ITEM_ALL -> {
+                task = db.searchAll(text.toString())
+            }
+            ITEM_KOREAN -> {
+                task = db.searchKorean(text.toString())
+            }
+            ITEM_ENGLISH -> {
+                task = db.searchEnglish(text.toString())
+            }
+        }
+        task?.addOnCompleteListener(object: OnCompleteListener<List<RemoteDatabase.FirebaseProduct>> {
+            override fun onComplete(t: Task<List<RemoteDatabase.FirebaseProduct>>) {
+                if (t.isSuccessful) {
+                    val res = t.result
+                    res?.forEach { print(it) }
+                } else {
+
+                }
+            }
+
+        })
     }
 }
