58c3c7cc53d0b08903d9f6f750ff01d24c176baf
cloudytheconqueror <cloudy5939@gmail.com>
2019-11-03 21:04:33 +0900

    feat: add basic ProductInfoActivity

---
+++ app/src/main/java/com/example/mcateam6/activities/ProductInfoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mcateam6.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mcateam6.R
+
+class ProductInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_info)
+    }
+}
