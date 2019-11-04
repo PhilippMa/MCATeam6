720a6eef018b40b60ecbf80980dd3faaa5854ad4
cloudytheconqueror <cloudy5939@gmail.com>
2019-11-04 19:52:50 +0900

    feat: add setOnClickListener to SearchItemListAdapter.getView

--- app/src/main/java/com/example/mcateam6/adapters/SearchItemListAdapter.kt
+++ app/src/main/java/com/example/mcateam6/adapters/SearchItemListAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.mcateam6.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import com.example.mcateam6.R
+import com.example.mcateam6.activities.ProductInfoActivity
 import com.example.mcateam6.database.RemoteDatabase
 
 class SearchItemListAdapter(val context: Context?, var itemList: List<RemoteDatabase.FirebaseProduct>?): BaseAdapter() {
@@ -17,6 +20,14 @@ class SearchItemListAdapter(val context: Context?, var itemList: List<RemoteData
 
         if (convertView==null) {
             view = LayoutInflater.from(context).inflate(R.layout.search_listview_item, null)
+            view.setOnClickListener {
+                val intent = Intent(context, ProductInfoActivity::class.java).apply {
+                    putExtra("englishName", item?.name_english)
+                    putExtra("koreanName", item?.name_korean)
+                    putExtra("description", item?.description)
+                }
+                context?.startActivity(intent)
+            }
             holder = ViewHolder(view)
             holder.tvNameKorean.text = item?.name_korean
             holder.tvNameEnglish.text = item?.name_english
