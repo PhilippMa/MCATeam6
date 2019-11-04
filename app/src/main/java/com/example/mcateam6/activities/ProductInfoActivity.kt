0f7ca4f7923ea053241062f827f2280435d8e907
cloudytheconqueror <cloudy5939@gmail.com>
2019-11-04 18:51:18 +0900

    feat: separate english and korean names, receive data from intent

--- app/src/main/java/com/example/mcateam6/activities/ProductInfoActivity.kt
+++ app/src/main/java/com/example/mcateam6/activities/ProductInfoActivity.kt
@@ -3,11 +3,23 @@ package com.example.mcateam6.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mcateam6.R
+import kotlinx.android.synthetic.main.activity_product_info.*
 
 class ProductInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_info)
+
+        val intent = this.intent
+        val englishName = intent.extras?.get("englishName")
+        val koreanName = intent.extras?.get("koreanName")
+        val description = intent.extras?.get("description")
+//        Leaving out for now
+//        val attributes = intent.extras?.get("attributes")
+
+        text_english_name.text = englishName.toString()
+        text_korean_name.text = koreanName.toString()
+        text_description.text = description.toString()
     }
 }
