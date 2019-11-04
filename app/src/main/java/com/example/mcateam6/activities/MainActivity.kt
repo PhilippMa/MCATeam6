81ade923747dbf294407a5c68611d3b5b696a990
Philipp Martens <philipp.martens98@gmail.com>
2019-11-04 18:41:44 +0900

    feat: improve MainActivity layout

--- app/src/main/java/com/example/mcateam6/activities/MainActivity.kt
+++ app/src/main/java/com/example/mcateam6/activities/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.mcateam6.activities
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mcateam6.R
@@ -27,6 +30,16 @@ class MainActivity : AppCompatActivity() {
             addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             statusBarColor = Color.TRANSPARENT
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById(R.id.container)
+        ) { _, insets ->
+            val container = findViewById<ConstraintLayout>(R.id.container)
+            val containerLayoutParams = container.layoutParams as ViewGroup.MarginLayoutParams
+            containerLayoutParams.setMargins(0, insets.systemWindowInsetTop, 0, 0)
+            container.layoutParams = containerLayoutParams
+            insets.consumeSystemWindowInsets()
         }
 
         if (!Utils.allPermissionsGranted(this)) {
