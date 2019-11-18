7351aee93b9543278928f796308b1e03f2f88ce7
Yuna Jeong <yoona3316@naver.com>
2019-11-18 12:02:39 +0900

    fix: delete useless imports

--- app/src/main/java/com/example/mcateam6/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/mcateam6/fragments/SearchFragment.kt
@@ -1,7 +1,6 @@
 package com.example.mcateam6.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
@@ -10,7 +9,6 @@ import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.PopupMenu
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
