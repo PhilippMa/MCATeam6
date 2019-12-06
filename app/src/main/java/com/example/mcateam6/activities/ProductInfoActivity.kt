13e250cfa490f83f07b40c4aa51d2990415182a6
cloudytheconqueror <cloudy5939@gmail.com>
2019-12-06 16:40:23 +0900

    feat: implement ingredient list on ProductInfoActivity

--- app/src/main/java/com/example/mcateam6/activities/ProductInfoActivity.kt
+++ app/src/main/java/com/example/mcateam6/activities/ProductInfoActivity.kt
@@ -5,16 +5,25 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.mcateam6.R
+import com.example.mcateam6.adapters.SearchItemAdapter
 import com.example.mcateam6.database.RemoteDatabase
 import kotlinx.android.synthetic.main.activity_product_info.*
 
 class ProductInfoActivity : AppCompatActivity() {
     val db = RemoteDatabase()
 
+    var itemList: MutableList<RemoteDatabase.FirebaseProduct>? = mutableListOf()
+    lateinit var itemAdapter: SearchItemAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_info)
+
+        itemAdapter = SearchItemAdapter(this, itemList)
+        recycler_ingredients.adapter = itemAdapter
+        recycler_ingredients.layoutManager = GridLayoutManager(this, 1)
 
         val intent = this.intent
         val id = intent.extras?.get("id").toString()
@@ -40,7 +49,7 @@ class ProductInfoActivity : AppCompatActivity() {
                 }
 
                 val imageTask = db.downloadImageSmall(id)
-                imageTask.addOnCompleteListener{it2 ->
+                imageTask.addOnCompleteListener{ it2 ->
                     if (it2.isSuccessful) {
                         val byteArray = it2.result
                         val bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
@@ -52,7 +61,15 @@ class ProductInfoActivity : AppCompatActivity() {
                     }
                 }
 
-//                recycler_ingredients.
+                val ingredientTask = product?.getIngredientProducts()
+                ingredientTask?.addOnCompleteListener { it3 ->
+                    if (it3.isSuccessful) {
+                        itemAdapter.updateWholeData(it3.result)
+                    } else {
+                        val toast = Toast.makeText(this, "Failed to load ingredients", Toast.LENGTH_SHORT)
+                        toast.show()
+                    }
+                }
             } else {
                 val toast = Toast.makeText(this, "Failed to load product", Toast.LENGTH_SHORT)
                 toast.show()
