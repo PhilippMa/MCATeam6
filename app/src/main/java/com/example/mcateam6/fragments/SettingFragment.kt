4425e3d318ef9537b479d5cec1f3b2141f19dcb2
dtaneli16 <dtaneli16@ku.edu.tr>
2019-12-07 17:33:37 +0900

    create Settings Fragment

--- app/src/main/java/com/example/mcateam6/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/mcateam6/fragments/SettingFragment.kt
@@ -5,14 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import com.example.mcateam6.R
 
-class SettingFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+class SettingFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences2, rootKey)
     }
+
 }
