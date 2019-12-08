9d680954eb5973ddd3d5fe704d56e5632c55fc15
dtaneli16 <dtaneli16@ku.edu.tr>
2019-12-08 15:29:44 +0900

    remove unused imports

--- app/src/main/java/com/example/mcateam6/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/mcateam6/fragments/SettingFragment.kt
@@ -1,10 +1,6 @@
 package com.example.mcateam6.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
 import com.example.mcateam6.R
 
